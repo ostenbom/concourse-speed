a5163549cc1fe242dc7dd2ed1cc27b584636d416
Oliver Stenbom <ostenbom@pivotal.io>
2018-09-20 17:49:50 +0100

    Converting data from query to heatmap data

--- spec/javascripts/speedmap_spec.js
+++ spec/javascripts/speedmap_spec.js
@@ -32,3 +32,110 @@ describe("the speedmap", function() {
     return d3.select("svg");
   }
 });
+
+describe("the data", function() {
+  var exampleData;
+
+  beforeEach(function() {
+    exampleData = JSON.parse(exampleDataString);
+  });
+
+  it("converts server data to map data", function() {
+    parsedEvents = parseBuildEventTimes(exampleData);
+    mapData = buildEventsToMapData(exampleData, 10, maxInBox);
+    console.log(mapData)
+    expect(mapData[0].Job).toBe(0);
+    expect(mapData[1].Job).toBe(1);
+    expect(mapData[mapData.length - 1].Job).toBe(4);
+  });
+});
+
+
+var exampleDataString = `
+[
+{
+"Build":"26",
+"Job":"guardian-containerd-xenial",
+"Status":"succeeded",
+"Start":"2018-09-13T16:25:23.107323+01:00",
+"End":"2018-09-13T16:39:37.263175+01:00"
+},
+{
+"Build":"3464",
+"Job":"guardian-windows-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T09:58:44.415586+01:00",
+"End":"2018-09-13T10:00:40.267257+01:00"
+},
+{
+"Build":"3468",
+"Job":"guardian-xenial-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T11:19:05.357905+01:00",
+"End":"2018-09-13T11:44:26.442093+01:00"
+},
+{
+"Build":"3466",
+"Job":"guardian-trusty-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T09:18:33.042908+01:00",
+"End":"2018-09-13T09:44:24.295419+01:00"
+},
+{
+"Build":"3467",
+"Job":"guardian-trusty-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T09:58:44.293686+01:00",
+"End":"2018-09-13T10:24:30.848283+01:00"
+},
+{
+"Build":"3469",
+"Job":"guardian-trusty-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T11:19:05.283632+01:00",
+"End":"2018-09-13T11:44:39.113786+01:00"
+},
+{
+"Build":"3465",
+"Job":"guardian-xenial-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T09:18:33.113619+01:00",
+"End":"2018-09-13T09:43:54.244709+01:00"
+},
+{
+"Build":"3466",
+"Job":"grootfs-xenial-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T09:58:44.529213+01:00",
+"End":"2018-09-13T10:01:01.267534+01:00"
+},
+{
+"Build":"3471",
+"Job":"guardian-trusty-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T12:39:13.330466+01:00",
+"End":"2018-09-13T13:05:01.233263+01:00"
+},
+{
+"Build":"3463",
+"Job":"guardian-windows-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T09:18:33.185654+01:00",
+"End":"2018-09-13T09:20:29.910194+01:00"
+},
+{
+"Build":"3470",
+"Job":"guardian-xenial-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T12:39:13.403216+01:00",
+"End":"2018-09-13T13:04:12.959186+01:00"
+},
+{
+"Build":"3475",
+"Job":"guardian-trusty-periodic",
+"Status":"succeeded",
+"Start":"2018-09-13T15:19:47.513394+01:00",
+"End":"2018-09-13T15:45:34.31831+01:00"
+}
+]
+`;
--- static/speedmap.js
+++ static/speedmap.js
@@ -11,3 +11,141 @@ function speedMap() {
 
   return map;
 }
+
+function maxInBox(boxData) {
+  var maxData = {};
+  maxData.Time = 0;
+
+  for (var i = 0; i < boxData.length; i++) {
+    var data = boxData[i];
+    if (data.Time > maxData.Time) {
+      maxData = data;
+    }
+  }
+
+  return maxData.Time
+}
+
+function getLatestStart(events) {
+  var latest = new Date('December 17, 1995 03:24:00');
+  for (var i = 0; i < events.length; i++) {
+    var event = events[i];
+    if (event.Start > latest) {
+      latest = event.Start;
+    }
+  }
+
+  return latest
+}
+
+function getEarliestStart(events) {
+  var earliest = Date.now();
+  for (var i = 0; i < events.length; i++) {
+    var event = events[i];
+    if (event.Start < earliest) {
+      earliest = event.Start;
+    }
+  }
+
+  return earliest
+}
+
+function getJobNames(events) {
+  var jobNames = [];
+  for (var i = 0; i < events.length; i++) {
+    var event = events[i];
+    if (!jobNames.includes(event.Job)) {
+      jobNames.push(event.Job);
+    }
+  }
+
+  return jobNames
+}
+
+function getEventsInBox(events, boxTime, boxTimeLength, jobName) {
+  var boxData = [];
+  for (var eventIndex = 0; eventIndex < events.length; eventIndex++) {
+    var event = events[eventIndex];
+    // console.log(event.Start.getTime(), boxTime, boxTime + boxTimeLength, jobName);
+    if (
+      event.Start.getTime() >= boxTime &&
+      event.Start.getTime() < (boxTime + boxTimeLength) &&
+      event.Job == jobName
+    ) {
+      // Warning optimisation do deepcopy
+      events.splice(eventIndex, 1);
+
+      var matchingEvent = {};
+
+      matchingEvent.Build = event.Build;
+      matchingEvent.Job = event.Job;
+      matchingEvent.Status = event.Status;
+      matchingEvent.Time = (event.End - event.Start) / 1000;
+
+      boxData.push(matchingEvent);
+    }
+
+  }
+
+  return boxData
+}
+
+function buildEventsToMapData(trueEvents, numBoxes, boxChooser) {
+  var events = trueEvents.slice(0);
+  var maxTime = getLatestStart(events);
+  var minTime = getEarliestStart(events);
+
+  var totalTime = (maxTime - minTime) / 1000;
+  var boxTimeLength = (totalTime / numBoxes) * 1000;
+
+  var jobNames = getJobNames(events);
+  var numJobs = jobNames.length;
+
+  var mapData = [];
+
+  var job, box;
+  for (job = 0; job < numJobs; job++) {
+    var boxTime = minTime.getTime();
+    for (box = 0; box < numBoxes; box++) {
+      var jobName = jobNames[job];
+      var eventsInBox = getEventsInBox(events, boxTime, boxTimeLength, jobName);
+      var boxValue = boxChooser(eventsInBox);
+      var boxData = {};
+
+      boxData.Job = job;
+      boxData.Box = box;
+      boxData.Value = boxValue;
+
+      mapData.push(boxData);
+
+      boxTime += boxTimeLength;
+    }
+  }
+
+  return mapData
+}
+
+function parseBuildEventTimes(events) {
+  for (var i = 0; i < events.length; i++) {
+    var event = events[i]
+    event.Start = new Date(event.Start);
+    event.End = new Date(event.End);
+  }
+
+  return events
+}
+
+function getJson(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+}
